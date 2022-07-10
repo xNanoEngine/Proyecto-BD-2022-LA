63fea9429f627a20c98a3a252e97946a8526dd35
Sebastian <sebastian.paredes01@alumnos.uach.cl>
2022-07-09 20:25:50 -0400

    crear db

---
+++ create_db.py
@@ -0,0 +1,31 @@
+import mariadb
+import sys
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="root",
+        password="root",
+        host="127.0.0.1",
+        port=3306
+    )
+
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
+
+# Create Database
+query_create = "CREATE DATABASE region_LosRios"
+cur.execute(query_create)
+cur.execute("USE region_LosRios")
+cur.execute("CREATE TABLE medio_de_prensa (id_medio INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(300), fecha_creacion DATE  , url_media VARCHAR(300), region VARCHAR(300), pais VARCHAR(300)), idioma")
+cur.execute("CREATE TABLE noticia (id_noticia INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(300), titulo VARCHAR(300), fecha_publicacion DATE, contenido TEXT)")
+cur.execute("CREATE TABLE dueno (id_dueno INT AUTO_INCREMENT PRIMARY KEY, nombre_dueno VARCHAR(300), tipo_dueno VARCHAR(300), fecha_adquisicion DATE)")
+cur.execute("CREATE TABLE autor (id_auto INT AUTO_INCREMENT PRIMARY KEY, nombre_autor VARCHAR(300)")
+cur.execute("CREATE TABLE persona (id_persona INT AUTO_INCREMET PRIMARY KEY, nombre_persona VARCHAR(300), nacionalidad VARCHAR(300), popularidad INT, profesion VARCHAR(300), fecha_nacimiento VARCHAR(300)")
+
+conn.commit() 
+conn.close()
