f2b3b110114efd8d88e8fb638b3178a87929fa9a
xNanoEngine <83073528+xNanoEngine@users.noreply.github.com>
2022-07-13 22:51:35 -0400

    Fixed

--- create_db.py
+++ create_db.py
@@ -5,7 +5,7 @@ import sys
 try:
     conn = mariadb.connect(
         user="root",
-        password="root",
+        password="0908",
         host="127.0.0.1",
         port=3306
     )
@@ -21,11 +21,11 @@ cur = conn.cursor()
 query_create = "CREATE DATABASE region_LosRios"
 cur.execute(query_create)
 cur.execute("USE region_LosRios")
-cur.execute("CREATE TABLE medio_de_prensa (id_medio INT AUTO_INCREMENT PRIMARY KEY, FOREIGN KEY (id_dueno) REFERENCES dueno(id_dueno), nombre VARCHAR(300), fecha_creacion DATE  , url_media VARCHAR(300), region VARCHAR(300), pais VARCHAR(300), idioma VARCHAR(300)")
-cur.execute("CREATE TABLE noticia (id_noticia INT AUTO_INCREMENT PRIMARY KEY, FOREIGN KEY (id_autor) REFERENCES autor(id_autor), FOREIGN KEY (id_medio) REFERENCES medio_de_prensa(id_medio)), url VARCHAR(300), titulo VARCHAR(300), fecha_publicacion DATE, contenido TEXT")
 cur.execute("CREATE TABLE dueno (id_dueno INT AUTO_INCREMENT PRIMARY KEY, nombre_dueno VARCHAR(300), tipo_dueno VARCHAR(300), fecha_adquisicion DATE)")
-cur.execute("CREATE TABLE autor (id_auto INT AUTO_INCREMENT PRIMARY KEY, nombre_autor VARCHAR(300)")
-cur.execute("CREATE TABLE persona (id_persona INT AUTO_INCREMET PRIMARY KEY, FOREIGN KEY (id_noticia) REFERENCES noticia(id_noticia), nombre_persona VARCHAR(300), nacionalidad VARCHAR(300), popularidad INT, profesion VARCHAR(300), fecha_nacimiento VARCHAR(300)")
+cur.execute("CREATE TABLE autor (id_autor INT AUTO_INCREMENT PRIMARY KEY, nombre_autor VARCHAR(300))")
+cur.execute("CREATE TABLE medio_de_prensa (id_medio INT AUTO_INCREMENT PRIMARY KEY,id_dueno INT, FOREIGN KEY (id_dueno) REFERENCES dueno(id_dueno), nombre VARCHAR(300), fecha_creacion DATE, url_media VARCHAR(300), region VARCHAR(300), pais VARCHAR(300), idioma VARCHAR(300))")
+cur.execute("CREATE TABLE noticia (id_noticia INT AUTO_INCREMENT PRIMARY KEY, id_autor INT ,FOREIGN KEY (id_autor) REFERENCES autor(id_autor), id_medio INT ,FOREIGN KEY (id_medio) REFERENCES medio_de_prensa(id_medio), url VARCHAR(300), titulo VARCHAR(300), fecha_publicacion DATE, contenido TEXT)")
+cur.execute("CREATE TABLE persona (id_persona INT AUTO_INCREMENT PRIMARY KEY, id_noticia INT, FOREIGN KEY (id_noticia) REFERENCES noticia(id_noticia), nombre_persona VARCHAR(300), nacionalidad VARCHAR(300), popularidad INT, profesion VARCHAR(300), fecha_nacimiento VARCHAR(300))")
 
 conn.commit() 
 conn.close()
